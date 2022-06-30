ebf4812f6d1f6e4f5c6c643ed67a97ed7d922985
Syed Abdul Aziz <92917995+syedazeez256@users.noreply.github.com>
2022-06-30 22:04:40 +0500

    var and let scopes in settiomeout in for loop

--- script.js
+++ script.js
@@ -48,31 +48,59 @@
 // func(x);
 // console.log(x);
 
-let arr1 = [1, 2, 3];
-let arr2 = [1, 5, 8, 0, 1, 2, 2, 3, 4, 5];
-let a;
+// To find the intersection of these two arrays ////
+// let arr1 = [1, 2, 3];
+// let arr2 = [1, 5, 8, 3, 4, 5];
 // for (let i = 0; i < arr1.length; i++) {
 //   for (let j = 0; j < arr2.length; j++) {
 //     if (arr1[i] === arr2[j]) {
-//       a.push(arr2[j]);
+//       var x = arr2;
 //     }
 //   }
 // }
-// a = [...arr1, ...arr2];
+// let a = [...arr1, ...arr2];
 // let myset = new Set(a);
-// console.log(myset);
-// console.log(arr1.findIndex());
+// a = [...myset];
+// console.log(a);
+// // console.log(arr1.findIndex());
 // console.log(arr1);
 
-arr2.sort();
-let sets = new Set(arr2);
-console.log(sets);
-let arr = [...sets];
-console.log(arr);
-let element;
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === arr[1]) {
-    element = arr[i];
-  }
+// Find the 2nd Last Smallest Number in arrays ////
+// arr2.sort();
+// let sets = new Set(arr2);
+// console.log(sets);
+// let arr = [...sets];
+// console.log(arr);
+// let element;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === arr[1]) {
+//     element = arr[i];
+//   }
+// }
+// console.log(element);
+
+// for (let i = 0; i <= 10; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+// console.log("script start");
+
+// setTimeout(() => {
+//   console.log("set time out");
+// }, 0);
+
+// Promise.resolve()
+//   .then(() => console.log("Promise resolve"))
+//   .then(() => console.log("Promise resolve 2"));
+
+// console.log("script end");
+
+function x() {
+  var i = 1;
+  setTimeout(function () {
+    console.log(i);
+  }, 1000);
 }
-console.log(element);
+x();
